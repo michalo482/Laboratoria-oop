6ee3baadefa5847538005072f778c5700ee90b20
dakko <dakkon@o2.pl>
2022-04-22 17:08:03 +0200

    nowe

---
+++ lab4/DynamicArray/DynamicArray.cs
@@ -0,0 +1,230 @@
+﻿using System.Collections;
+
+namespace DynamicArray
+{
+    public class DynamicArray<T> : ICollection<T>, IEnumerator<T>, IDisposable
+    {
+        private T[]? array;
+        private int count;
+        private int index = -1;
+        
+
+        public DynamicArray()
+        {
+            array = new T[0];                       
+        }
+
+        public DynamicArray(int capacity) : this()
+        {
+            array = new T[capacity];
+            count = capacity;
+        }
+        
+        public T? this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+
+       public int Capacity
+        {
+            get
+            {
+                return array.Length;
+            }
+        }
+
+        public virtual T Current
+        {
+            get
+            {
+                try
+                {
+                    return (T)array[index];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+
+
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public void Add(T item)
+        {
+            if (array.Length == count)
+            {
+                T[] newArray = new T[array.Length + 1];
+                Array.Copy(array, newArray, array.Length);
+                array = newArray;
+            }
+            array[count] = item;
+            count++;
+        }
+       
+
+        public void Clear()
+        {
+            array = new T[0];
+            count = 0;
+        }
+
+        public virtual bool Contains(T item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            Array.Copy(this.array, array, this.array.Length);
+        }
+
+        public virtual IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (array[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Insert(int index, T item)
+        {
+            if (index < 0 || index > count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (array.Length == count)
+            {
+                T[] newArray = new T[array.Length + 1];
+                Array.Copy(array, newArray, array.Length);
+                array = newArray;
+            }
+            Array.Copy(array, index, array, index + 1, count - index);
+            array[index] = item;
+            count++;
+        }
+
+        public void ChangeAt(int index, T item)
+        {
+            if (index < 0 || index > count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            array[index] = item;    
+        }
+
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index == -1)
+            {
+                return false;
+            }
+            array = array.Where((val, idx) => idx != index).ToArray();
+            count--;
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            array = array.Where((val, idx) => idx != index).ToArray();
+            count--;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Dispose()
+        {                       
+            GC.SuppressFinalize(this);
+        }
+
+        public bool MoveNext()
+        {
+            index++;
+            return (index < count);
+        }
+
+        public void Reset()
+        {
+            index = -1;
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            for (int i = 0; i < count; i++)
+            {
+                result += array[i] + " ";                          
+            }
+            return result;
+        }
+
+        public T[] ToArray()
+        {
+            T[] result = new T[count];
+            Array.Copy(array, result, count);
+            return result;
+        }
+
+        public void Sort()
+        {
+            Array.Sort(array, 0, count);
+        }
+
+    }
+}
---
+++ lab4/DynamicArrayApp/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DynamicArray;
+
+DynamicArray<int> numbers = new DynamicArray<int>();
+DynamicArray<Object> numbers2 = new DynamicArray<Object>(5);
+
+Object obj = new Object();
+
+numbers.Add(1);
+numbers.Add(2);
+numbers.Add(3);
+numbers.Add(4);
+numbers.Add(5);
+
+numbers2.Insert(0, obj);
+
+Console.WriteLine("count: " + numbers.Count);
+
+numbers.Insert(2, 100);
+numbers.ChangeAt(1, 14);
+Console.WriteLine("-----");
+foreach (int number in numbers)
+{
+    Console.WriteLine(number);
+}
+Console.WriteLine("-----");
+
+Console.WriteLine("count: " + numbers.Count);
+
+Console.WriteLine(numbers[2]);
+
+numbers.Remove(5);
+
+Console.WriteLine("count: " + numbers.Count);
+
+numbers.RemoveAt(0);
+
+Console.WriteLine("count: " + numbers.Count);
+
+Console.WriteLine(numbers[0]);
+
+int num = numbers.IndexOf(5);
+
+Console.WriteLine(num);
+
+Console.WriteLine();
+
+Console.WriteLine("count: " + numbers.Count);
+
+foreach (var num2 in numbers2)
+{
+    Console.WriteLine("----");
+    Console.WriteLine(num2);
+}
+
+Console.WriteLine("count: " + numbers2.Count);
+
+Console.WriteLine(numbers2[3]);
+
