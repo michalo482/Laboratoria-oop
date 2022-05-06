aceb946dd230107236c14629b82462b4757bc527
michalo482 <77701451+michalo482@users.noreply.github.com>
2022-05-06 20:53:08 +0200

    Add files via upload

---
+++ lab4_zadanie/lab4_zad_app/Program.cs
@@ -0,0 +1,21 @@
+﻿using lab4_zad;
+
+BothThings myNode = new("D:/test/");
+
+myNode.PrintNode();
+OrderByNameAndSize byName = new(myNode.files);
+byName.PrintByName();
+byName.PrintBySize();
+OrderByFirstLetter byFirstLetter = new(myNode.files);
+byFirstLetter.PrintByFirstLetter();
+ByTypes byTypes = new(myNode.files);
+byTypes.Print();
+BySizes bySizes = new(myNode.files);
+bySizes.PrintBySizes();
+ByExtension byExtension = new(myNode.files);
+byExtension.PrintByExtension();
+
+
+
+
+
