615098d89b3f9415ac4cc8f429c64fadb5b04b37
dakko <dakkon@o2.pl>
2022-05-06 20:37:56 +0200

    lab4_zad

---
+++ lab1/HelloConsoleAppBatch/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace HelloConsoleAppBatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program na powintanie.");
+
+            string imie;
+            string nazwisko;
+            int wiek;
+
+            if (args.Length == 0)
+            {
+                Console.Write("Podaj swoje imie: ");
+                imie = Console.ReadLine();
+
+                Console.Write("Podaj swoje nazwisko: ");
+                nazwisko = Console.ReadLine();
+
+                Console.WriteLine("Witaj " + imie + " " + nazwisko);
+                Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiscie nazywa sie {1}?", imie, nazwisko);
+
+                Console.WriteLine("Podaj swoj wiek: ");
+                wiek = Convert.ToInt32(Console.ReadLine());
+
+                if (wiek < 0)
+                {
+                    Console.WriteLine("Bledne dane!");
+                }
+                else if (wiek < 67)
+                {
+                    Console.WriteLine("do emerytury zostalo ci {0} lat", 67 - wiek);
+                }
+                else
+                {
+                    Console.WriteLine("jestes emerytem");
+                }
+            }
+            else if (args.Length == 1)
+            {
+                imie = args[0];
+
+                Console.Write("Podaj swoje nazwisko: ");
+                nazwisko = Console.ReadLine();
+
+                Console.WriteLine("Witaj " + imie + " " + nazwisko);
+                Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiscie nazywa sie {1}?", imie, nazwisko);
+
+                Console.WriteLine("Podaj swoj wiek: ");
+                wiek = Convert.ToInt32(Console.ReadLine());
+
+                if (wiek < 0)
+                {
+                    Console.WriteLine("Bledne dane!");
+                }
+                else if (wiek < 67)
+                {
+                    Console.WriteLine("do emerytury zostalo ci {0} lat", 67 - wiek);
+                }
+                else
+                {
+                    Console.WriteLine("jestes emerytem");
+                }
+            }
+            else if (args.Length == 2)
+            {
+
+                imie = args[0];
+
+                nazwisko = args[1];
+
+                Console.WriteLine("Witaj " + imie + " " + nazwisko);
+                Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiscie nazywa sie {1}?", imie, nazwisko);
+
+                Console.WriteLine("Podaj swoj wiek: ");
+                wiek = Convert.ToInt32(Console.ReadLine());
+
+                if (wiek < 0)
+                {
+                    Console.WriteLine("Bledne dane!");
+                }
+                else if (wiek < 67)
+                {
+                    Console.WriteLine("do emerytury zostalo ci {0} lat", 67 - wiek);
+                }
+                else
+                {
+                    Console.WriteLine("jestes emerytem");
+                }
+            }
+            else if (args.Length == 3)
+            {
+                imie = args[0];
+                nazwisko = args[1];
+                wiek = Convert.ToInt32(args[2]);
+                Console.WriteLine("Witaj " + imie + " " + nazwisko);
+                Console.WriteLine("Witaj {0} {1}. Czy Pan {0} rzeczywiscie nazywa sie {1}?", imie, nazwisko);
+                if (wiek < 0)
+                {
+                    Console.WriteLine("Bledne dane!");
+                }
+                else if (wiek < 67)
+                {
+                    Console.WriteLine("do emerytury zostalo ci {0} lat", 67 - wiek);
+                }
+                else
+                {
+                    Console.WriteLine("jestes emerytem");
+                }
+            }
+        }
+    }
+}
---
+++ lab1/HelloConsoleVBDialog/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace HelloConsoleVBDialog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program na powitanie.");
+
+            string imie = Interaction.InputBox("Podaj imie", "Okienko imie");
+            if (imie == "")
+            {
+                Console.WriteLine("Koniec");
+                System.Environment.Exit(1);
+            }
+            Console.WriteLine("podano imie " + imie);
+
+            string nazwisko = Interaction.InputBox("Podaj nazwisko", "Okienko nazwisko");
+            if (nazwisko == "")
+            {
+                Console.WriteLine("Koniec");
+                System.Environment.Exit(1);
+            }
+            Console.WriteLine("podano nazwisko " + nazwisko);
+
+            Interaction.MsgBox("Witaj " + imie + " " + nazwisko);
+            Interaction.MsgBox(string.Format("Witaj {0} {1}. Czy Pan {0} zeczywiscie nazywa sie {1}?", imie, nazwisko));
+
+            string wiekTemp = Interaction.InputBox("Podaj wiek", "Okienko wiek");
+            int wiek;
+            if (wiekTemp == "")
+            {
+                Console.WriteLine("Koniec");
+                System.Environment.Exit(1);
+            }
+
+
+            wiek = Convert.ToInt32(wiekTemp);
+
+
+            if (wiek < 67)
+            {
+                Interaction.MsgBox("do emerytury zostalo ci " + (67 - wiek) + " lat", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko emerytura");
+            }
+            else
+            {
+                Interaction.MsgBox("Jestes na emeryturze", MsgBoxStyle.OkOnly | MsgBoxStyle.Information, "Okienko emerytura");
+            }
+        }
+    }
+}
